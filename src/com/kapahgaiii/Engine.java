c924726130752fa309999ca4c3058a5bbb09b39f
KaPaHgaIII <denis-semenov.job@yandex.ru>
2014-07-16 23:06:46 +0400

    It's done. Yes, it's not ideal, but it works! And it has menu, and word endings sounds better.

--- src/com/kapahgaiii/Engine.java
+++ src/com/kapahgaiii/Engine.java
@@ -4,79 +4,73 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class Engine {
     private char[] russianAlphabet = {'а', 'е', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т'}; //most-used characters
-    private HashMap<String, Segment> data = new HashMap<String, Segment>();
-    Random randomizer = new Random();
+    private HashMap<String, Segment> data = new HashMap<String, Segment>();//our char-links
+    Random randomizer = new Random(System.nanoTime());
 
-    public class Segment {
+    public class Segment { //pair of characters with the list of possible next chars
         String subs;
-        LinkedHashMap<String, Integer> nextChars = new LinkedHashMap<String, Integer>();
-        String canBeEndWith = null;
-        int randomSum = 0;
+        HashSet<String> nextChars = new HashSet<String>();
+        HashSet<String> canBeEndWith = new HashSet<String>();//we need it for good sounding the rest of a word
 
         public Segment(String subs) {
             this.subs = subs;
         }
-
-        public String getRandom() {
-            int index = randomizer.nextInt(randomSum);
-            int sum = 0;
-            Map.Entry<String, Integer> pair;
-            Iterator<Map.Entry<String, Integer>> it = nextChars.entrySet().iterator();
-            do {
-                pair = it.next();
-                sum += pair.getValue();
-            } while (it.hasNext() && sum < index);
-
-            return pair.getKey();
-        }
-
     }
 
-    public Engine(String filename) throws IOException {
-        //reading our data
+    public Engine() {
+        readData("male_names.txt");
+        readData("female_names.txt");
+    }
+
+    public void readData(String filename) {
         try {
             BufferedReader in = new BufferedReader(new FileReader(filename));
-
             String s;
             while ((s = in.readLine()) != null) {
                 s = s.toLowerCase();
                 for (int i = 0; i < s.length() - 2; i++) {
+                    //get substrings
                     String subs = s.substring(i, i + 2);
                     String nextChar = s.substring(i + 2, i + 3);
-                    if (data.containsKey(subs)) {
-                        if (data.get(subs).nextChars.containsKey(nextChar)) {
-                            data.get(subs).nextChars.put(nextChar, data.get(subs).nextChars.get(nextChar) + 1);
-                        } else {
-                            data.get(subs).nextChars.put(nextChar, 1);
-                        }
-                    } else {
+                    //adding pair
+                    if (!data.containsKey(subs)) {
                         data.put(subs, new Segment(subs));
                     }
+                    //adding nextChar
+                    if (!data.get(subs).nextChars.contains(nextChar)) {
+                        data.get(subs).nextChars.add(nextChar);
+                    }
+                    //adding ending
                     if (i == s.length() - 4) {
-                        data.get(subs).canBeEndWith = s.substring(i + 2);
+                        String end = s.substring(i + 2);
+                        if (!data.get(subs).canBeEndWith.contains(end)) {
+                            data.get(subs).canBeEndWith.add(end);
+                        }
                     }
                 }
             }
         } catch (FileNotFoundException e) {
             System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
         }
-        //counting random sums
-        for (HashMap.Entry<String, Segment> outPair : data.entrySet()) {
-            for (Map.Entry<String, Integer> inPair : outPair.getValue().nextChars.entrySet()) {
-                outPair.getValue().randomSum += inPair.getValue();
-            }
-        }
+    }
 
+    public void clearData() {
+        data = new HashMap<String, Segment>();
     }
 
     public void printData() {
-        for (HashMap.Entry<String, Segment> outPair : data.entrySet()) {
-            for (Map.Entry<String, Integer> inPair : outPair.getValue().nextChars.entrySet()) {
-                System.out.println(outPair.getKey() + " " + inPair.getKey() + " " + inPair.getValue());
+        for (HashMap.Entry<String, Segment> out : data.entrySet()) {
+            for (String s : out.getValue().nextChars) {
+                System.out.println(out.getKey() + " " + s);
             }
         }
     }
@@ -85,45 +79,63 @@ public class Engine {
         String name; //our result
 
         Object[] keys = data.keySet().toArray();
-        name = (String) keys[randomizer.nextInt(keys.length)]; //start width a random 2 symbols
+        do {
+            name = (String) keys[randomizer.nextInt(keys.length)]; //start width a random 2 symbols, don't start with Ъ, Ы, Ь
+        }
+        while (name.substring(0, 1).equals("ь") || name.substring(0, 1).equals("ъ") || name.substring(0, 1).equals("ы"));
 
-        while (name.length() < length - 1) {
+        while (name.length() < length - 2) {//each iteration adds one character, last two will be added later
+            String lastSymbols = name.substring(name.length() - 2);//we take two last characters
+            String c = null;//and add suitable for it
 
-            String c = null;
-
-            String lastSymbols = name.substring(name.length() - 2);
-            if (data.containsKey(lastSymbols)) {
-
+            if (data.containsKey(lastSymbols)) { //searching for c
                 Segment next = data.get(lastSymbols);
-
+                String[] chars = stringSetToMixedArray(next.nextChars);
                 int i = 0;
-
-                Object[] chars = next.nextChars.keySet().toArray();
-
-                while (i < chars.length) { //yes, that's awful
-                    String o = next.getRandom();
+                while (i < chars.length) {
+                    String o = chars[i];
                     if (data.containsKey(name.substring(name.length() - 1) + o)) {
                         c = o;
+                        break;
                     }
                     i++;
                 }
             }
-            if (c == null) {
+            if (c == null) {//if not found, use one of the most-used characters
                 c = Character.toString(russianAlphabet[randomizer.nextInt(russianAlphabet.length - 1)]);
-                //break;
             }
 
             name += c;
         }
 
-        if (data.containsKey(name.substring(name.length() - 2)) && data.get(name.substring(name.length() - 2)).canBeEndWith != null) {
-            name += data.get(name.substring(name.length() - 2)).canBeEndWith;
-        } else {
+        //make ending
+        if (data.containsKey(name.substring(name.length() - 2)) && !data.get(name.substring(name.length() - 2)).canBeEndWith.isEmpty()) {
+            Object[] end = data.get(name.substring(name.length() - 2)).canBeEndWith.toArray();
+            name += end[randomizer.nextInt(end.length)];
+        } else {//if haven't suitable use random pair
             name += (String) keys[randomizer.nextInt(keys.length)];
         }
 
+        if (name.length() > length) { //it can be truth when length < 4
+            name = name.substring(name.length() - length);
+        }
 
         return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 
+    public String[] stringSetToMixedArray(Set<String> set) {
+        String[] array = new String[set.size()];
+        int i = 0;
+        for (String s : set) {
+            array[i] = s;
+            i++;
+        }
+        for (int j = 0; j < array.length; j++) {
+            int k = randomizer.nextInt(array.length);
+            String tmp = array[j];
+            array[j] = array[k];
+            array[k] = tmp;
+        }
+        return array;
+    }
 }
--- src/com/kapahgaiii/Menu.java
+++ src/com/kapahgaiii/Menu.java
@@ -6,17 +6,71 @@ import java.io.InputStreamReader;
 
 public class Menu {
     private byte option;
+    private byte length = 5;
+    private String genderText = "M&F";
+    private Engine engine;
 
-    public void run(Engine engine) {
+    public Menu(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void run() {
+        System.out.println("Hello!");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
-            while ((option = Byte.parseByte(reader.readLine())) != -1) {
-                System.out.println("hello");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println(e);
+            do {
+                try {
+                    System.out.print("Type the number of a command (0 for help): ");
+                    option = Byte.parseByte(reader.readLine());
+                    apply(reader);
+                } catch (NumberFormatException e) {
+                    System.out.println("Incorrect input");
+                }
+
+            } while (option != -1);
         } catch (IOException e) {
             System.out.println(e);
+        }
+    }
+
+    private void apply(BufferedReader reader) throws IOException, NumberFormatException {
+        switch (option) {
+            case -1:
+                System.out.println("Goodbye!");
+                break;
+            case 0:
+                System.out.println("0) Help");
+                System.out.println("1) Generate name");
+                System.out.println("2) Choose name length (now " + length + ")");
+                System.out.println("3) Choose gender (now " + genderText + ")");
+                System.out.println("-1) Exit");
+                break;
+            case 1:
+                System.out.println(engine.generateName(length));
+                break;
+            case 2:
+                System.out.print("Enter length: ");
+                length = Byte.parseByte(reader.readLine());
+                break;
+            case 3:
+                System.out.print("Enter M, F or M&F: ");
+                genderText = reader.readLine();
+                if (genderText.equals("M")) {
+                    engine.clearData();
+                    engine.readData("male_names.txt");
+                } else if (genderText.equals("F")) {
+                    engine.clearData();
+                    engine.readData("female_names.txt");
+                } else {
+                    genderText = "M&F";
+                    engine.clearData();
+                    engine.readData("male_names.txt");
+                    engine.readData("female_names.txt");
+                }
+                break;
+            default:
+                System.out.println("Command not found");
+                break;
         }
     }
 }
