81561249a2fe3e1a2cf6fab09d6754485bd36eda
KaPaHgaIII <denis-semenov.job@yandex.ru>
2014-07-16 17:44:31 +0400

    This version considers probabilities, but decreased output quality. In future versions there won't be probabilities, and as a result code will become easier.

--- src/com/kapahgaiii/Engine.java
+++ src/com/kapahgaiii/Engine.java
@@ -4,24 +4,40 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Random;
+import java.util.*;
 
 public class Engine {
-
+    private char[] russianAlphabet = {'а', 'е', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т'}; //most-used characters
     private HashMap<String, Segment> data = new HashMap<String, Segment>();
+    Random randomizer = new Random();
 
     public class Segment {
         String subs;
-        HashMap<String, Integer> nextChars = new HashMap<String, Integer>();
+        LinkedHashMap<String, Integer> nextChars = new LinkedHashMap<String, Integer>();
         String canBeEndWith = null;
+        int randomSum = 0;
 
         public Segment(String subs) {
             this.subs = subs;
         }
+
+        public String getRandom() {
+            int index = randomizer.nextInt(randomSum);
+            int sum = 0;
+            Map.Entry<String, Integer> pair;
+            Iterator<Map.Entry<String, Integer>> it = nextChars.entrySet().iterator();
+            do {
+                pair = it.next();
+                sum += pair.getValue();
+            } while (it.hasNext() && sum < index);
+
+            return pair.getKey();
+        }
+
     }
 
     public Engine(String filename) throws IOException {
+        //reading our data
         try {
             BufferedReader in = new BufferedReader(new FileReader(filename));
 
@@ -40,20 +56,26 @@ public class Engine {
                     } else {
                         data.put(subs, new Segment(subs));
                     }
-                    if (i == s.length() - 3) {
-                        data.get(subs).canBeEndWith = nextChar;
+                    if (i == s.length() - 4) {
+                        data.get(subs).canBeEndWith = s.substring(i + 2);
                     }
                 }
             }
-
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
+        //counting random sums
+        for (HashMap.Entry<String, Segment> outPair : data.entrySet()) {
+            for (Map.Entry<String, Integer> inPair : outPair.getValue().nextChars.entrySet()) {
+                outPair.getValue().randomSum += inPair.getValue();
+            }
+        }
+
     }
 
     public void printData() {
         for (HashMap.Entry<String, Segment> outPair : data.entrySet()) {
-            for (HashMap.Entry<String, Integer> inPair : outPair.getValue().nextChars.entrySet()) {
+            for (Map.Entry<String, Integer> inPair : outPair.getValue().nextChars.entrySet()) {
                 System.out.println(outPair.getKey() + " " + inPair.getKey() + " " + inPair.getValue());
             }
         }
@@ -62,36 +84,46 @@ public class Engine {
     public String generateName(int length) {
         String name; //our result
 
-        Random generator = new Random();
         Object[] keys = data.keySet().toArray();
-        name = (String) keys[generator.nextInt(keys.length)]; //start width a random 2 symbols
+        name = (String) keys[randomizer.nextInt(keys.length)]; //start width a random 2 symbols
 
-        while (name.length() < length) {
+        while (name.length() < length - 1) {
 
             String c = null;
 
-            Segment next = data.get(name.substring(name.length() - 2));
+            String lastSymbols = name.substring(name.length() - 2);
+            if (data.containsKey(lastSymbols)) {
 
-            int i = 0;
+                Segment next = data.get(lastSymbols);
 
-            Object[] chars = next.nextChars.keySet().toArray();
+                int i = 0;
 
-            while (i < chars.length) { //yes, that's awful
-                String o = (String) (chars[generator.nextInt(chars.length)]);
-                if (data.containsKey(name.substring(name.length() - 1) + o)) {
-                    c = o;
+                Object[] chars = next.nextChars.keySet().toArray();
+
+                while (i < chars.length) { //yes, that's awful
+                    String o = next.getRandom();
+                    if (data.containsKey(name.substring(name.length() - 1) + o)) {
+                        c = o;
+                    }
+                    i++;
                 }
-                i++;
             }
-
             if (c == null) {
-                break;
+                c = Character.toString(russianAlphabet[randomizer.nextInt(russianAlphabet.length - 1)]);
+                //break;
             }
 
             name += c;
         }
 
-        return name;
+        if (data.containsKey(name.substring(name.length() - 2)) && data.get(name.substring(name.length() - 2)).canBeEndWith != null) {
+            name += data.get(name.substring(name.length() - 2)).canBeEndWith;
+        } else {
+            name += (String) keys[randomizer.nextInt(keys.length)];
+        }
+
+
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 
 }
